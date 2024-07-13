7ea8c768f20d9ef68866a1620f00de00c5c7eb89
vigna1234 <vignakodali5@gmail.com>
2024-07-12 19:35:04 -0700

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+        Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          <Divider />
+          {toggle === 'web projects' ?
+            <ToggleButton active value="web projects" onClick={() => setToggle('web projects')}>WEB PROJECTS</ToggleButton>
+            :
+            <ToggleButton value="web projects" onClick={() => setToggle('web projects')}>WEB PROJECTS</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'machine learning' ?
+            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            :
+            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+          }
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
